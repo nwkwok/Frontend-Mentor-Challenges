f77a907a7bac58f2f06bd21631c2455fac22de85
nwkwok <nwkwok@gmail.com>
2020-11-17 22:21:35 -0800

    make accordion and media queries

--- faq-accordion-card-main/dist/index.dev.js
+++ faq-accordion-card-main/dist/index.dev.js
@@ -1,9 +1,27 @@
 "use strict";
 
-var button = document.getElementById('test');
-var arrow = document.querySelector('.arrow');
-var answer = document.querySelector('.card-container-content-answer');
-button.addEventListener('click', function () {
-  arrow.classList.toggle('active');
-  answer.classList.toggle('active');
-});
+var question = document.getElementsByClassName('card-container-content-question');
+var answer = document.getElementsByClassName('card-container-content-answer');
+var arrow = document.getElementsByClassName('arrow');
+
+var _loop = function _loop(i) {
+  question[i].addEventListener('click', function () {
+    answer[i].classList.toggle('active');
+    arrow[i].classList.toggle('active');
+  });
+};
+
+for (var i = 0; i < question.length; i++) {
+  _loop(i);
+} // for (let i = 0; i<accordion.length; i++) {
+//     accordion[i].addEventListener('click', () =>{
+//         this.classList.toggle('active')
+//     })
+// }
+// const button = document.getElementById('test');
+// const arrow = document.querySelector('.arrow')
+// const answer = document.querySelector('.card-container-content-answer')
+// button.addEventListener('click', () => {
+//     arrow.classList.toggle('active');
+//     answer.classList.toggle('active');
+// });
--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -1,9 +1,27 @@
-const button = document.getElementById('test');
-const arrow = document.querySelector('.arrow')
-const answer = document.querySelector('.card-container-content-answer')
+const question = document.getElementsByClassName('card-container-content-question');
+const answer = document.getElementsByClassName('card-container-content-answer')
+const arrow = document.getElementsByClassName('arrow')
 
-button.addEventListener('click', () => {
-    arrow.classList.toggle('active');
-    answer.classList.toggle('active');
-});
+for (let i = 0; i<question.length; i++) {
+question[i].addEventListener('click', () => {
+    answer[i].classList.toggle('active');
+    arrow[i].classList.toggle('active');
+})
+}
+
+
+// for (let i = 0; i<accordion.length; i++) {
+//     accordion[i].addEventListener('click', () =>{
+//         this.classList.toggle('active')
+//     })
+// }
+
+// const button = document.getElementById('test');
+// const arrow = document.querySelector('.arrow')
+// const answer = document.querySelector('.card-container-content-answer')
+
+// button.addEventListener('click', () => {
+//     arrow.classList.toggle('active');
+//     answer.classList.toggle('active');
+// });
  
