82e69f4c9a30c84b25ae720cf49f520e16837196
nwkwok <nwkwok@gmail.com>
2020-11-19 22:28:51 -0800

    Delete config.js

--- ip-address-tracker-master/config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    mapbox_connect: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibndrd29rIiwiYSI6ImNraG9mNmJodTAwdmgydW56M3NkY2pkM2IifQ.BL_caXwKE6SBqpv2PFaFrw',
-    geo: 'at_PPBeNoLZOsvYSOewoAROhI9b1VRyX'
-}
